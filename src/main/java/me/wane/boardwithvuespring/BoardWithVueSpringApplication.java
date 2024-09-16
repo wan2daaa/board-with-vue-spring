1afcb1303e284ce724b92af18f929a7aa44cac97
wan2daaa <wan2daaa@gmail.com>
2024-09-17 01:03:32 +0900

    init project

---
+++ src/main/java/me/wane/boardwithvuespring/BoardWithVueSpringApplication.java
@@ -0,0 +1,13 @@
+package me.wane.boardwithvuespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardWithVueSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardWithVueSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/wane/boardwithvuespring/BoardWithVueSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package me.wane.boardwithvuespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardWithVueSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
